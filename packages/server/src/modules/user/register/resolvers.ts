68a31f774dd2859102ece06b44d4268194885225
Wang Weijing <you@example.com>
2020-03-24 16:39:29 +1100

    share code from 'common' to 'server'

--- packages/server/src/modules/user/register/resolvers.ts
+++ packages/server/src/modules/user/register/resolvers.ts
@@ -1,35 +1,23 @@
-import * as yup from "yup";
+import { validUserSchema } from "@airbnb/common";
 
-import { ResolverMap } from "../../../types/graphql-utils";
 import { User } from "../../../entity/User";
+import { ResolverMap } from "../../../types/graphql-utils";
 import { formatYupError } from "../../../utils/formatYupError";
-import {
-  duplicateEmail,
-  emailNotLongEnough,
-  invalidEmail
-} from "./errorMessages";
-import { registerPasswordValidation } from "../../../yupSchemas";
+import { duplicateEmail } from "./errorMessages";
+
 // import { createConfirmEmailLink } from "../../utils/createConfirmEmailLink";
 // import { sendEmail } from "../../utils/sendEmail";
-
-const schema = yup.object().shape({
-  email: yup
-    .string()
-    .min(3, emailNotLongEnough)
-    .max(255)
-    .email(invalidEmail),
-  password: registerPasswordValidation
-});
 
 export const resolvers: ResolverMap = {
   Mutation: {
     register: async (
       _,
+      // tslint:disable-next-line: trailing-comma
       args: GQL.IRegisterOnMutationArguments
       // { redis, url }
     ) => {
       try {
-        await schema.validate(args, { abortEarly: false });
+        await validUserSchema.validate(args, { abortEarly: false });
       } catch (err) {
         return formatYupError(err);
       }
